d019747d1b34b0aaf9f1833420373e47c1e356fb
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-03-29 20:46:35 +0700

    update backedn

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,26 @@ fetchData("learning").then((response) => {
 
   learning.innerHTML = html
 });
+
+fetchData("role").then((response) => {
+    let role = document.querySelector("#roles .col-md-4");
+    let roleAbout = document.querySelector("#roles #roleAbout div");
+    let roleAdvantage = document.querySelector("#roles #roleAdvantage div");
+    let rolePurpose = document.querySelector("#roles #rolePurpose div");
+    let html = '';
+    response.map((res) => {
+        html += `
+                <a href="#" class="d-block mt-5 ${res.active == true ? 'active' : ''}">
+                    <img src="/assets/icons/${res.icon}" alt="">
+                    <span>${res.name}</span>
+                </a>
+            `;
+    });
+
+    let data = response.filter((res) => res.active == true) 
+    
+    roleAbout.textContent = data[0].feature.about
+    roleAdvantage.textContent = data[0].feature.advantage
+    rolePurpose.textContent = data[0].feature.purpose
+    role.innerHTML = html
+});
