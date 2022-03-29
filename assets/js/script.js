dca8d6a71b913e6670a5f19acdc24be91e96f90e
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-03-29 20:02:59 +0700

    update fake backend

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
 fetchData("learning").then((response) => {
-  console.log(response); // fetched response
+  let learning = document.getElementById("learnings");
+  let html = '';
+  response.map((res) => {
+    html += `
+            <div class="col-md-3 m-2 card d-flex p-2 text-center align-items-center align-middle">
+                <img src="/assets/icons/${res.icon}" alt="" width="50">
+                <p class="mt-2">${res.name}</p>
+            </div>
+        `;
+  });
+
+  learning.innerHTML = html
 });
