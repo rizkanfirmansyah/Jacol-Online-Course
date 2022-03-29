a1e375757b954c24961c29de7a568deeb627e963
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-03-29 21:13:44 +0700

    update role

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,10 +18,12 @@ fetchData("role").then((response) => {
     let roleAbout = document.querySelector("#roles #roleAbout div");
     let roleAdvantage = document.querySelector("#roles #roleAdvantage div");
     let rolePurpose = document.querySelector("#roles #rolePurpose div");
+    let title = document.querySelector("#section-role h1 span");
+    let subtitle = document.querySelector("#section-role h1 b");
     let html = '';
     response.map((res) => {
         html += `
-                <a href="#" class="d-block mt-5 ${res.active == true ? 'active' : ''}">
+                <a href="#" onclick="return changeRole(${res.id}, this)" data-id="${res.id}" class="d-block mt-5 ${res.active == true ? 'active' : ''}">
                     <img src="/assets/icons/${res.icon}" alt="">
                     <span>${res.name}</span>
                 </a>
@@ -33,5 +35,34 @@ fetchData("role").then((response) => {
     roleAbout.textContent = data[0].feature.about
     roleAdvantage.textContent = data[0].feature.advantage
     rolePurpose.textContent = data[0].feature.purpose
+    title.textContent = data[0].name
+    subtitle.textContent = data[0].name
     role.innerHTML = html
 });
+
+function changeRole(id, elem){
+    fetchData("role").then((response) => {
+        let roleAbout = document.querySelector("#roles #roleAbout div");
+        let roleAdvantage = document.querySelector("#roles #roleAdvantage div");
+        let rolePurpose = document.querySelector("#roles #rolePurpose div");
+        let title = document.querySelector("#section-role h1 span");
+        let subtitle = document.querySelector("#section-role h1 b");
+    
+        let data = response.filter((res) => res.id == id) 
+        
+        roleAbout.textContent = data[0].feature.about
+        roleAdvantage.textContent = data[0].feature.advantage
+        rolePurpose.textContent = data[0].feature.purpose
+        title.textContent = data[0].name
+        subtitle.textContent = data[0].name
+    });
+
+    var a = document.querySelectorAll("#roles .col-md-4 a");
+    for (i = 0; i < a.length; i++) {
+        a[i].classList.remove('active')
+    }
+    elem.classList.add('active');
+
+    return false;
+}
+
